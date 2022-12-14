0e796bd9ce5836b8d27564c2e4de5403068cbddb
tatangracheva <grachtany@yandex.ru>
2022-12-14 17:26:54 +0300

    50

---
+++ task50/Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void ShowArray(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] CreateRandomArray(int rows, int coluns, int LeftRange, int RightRange)
+{
+    int[,] array = new int[rows, coluns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < coluns; j++)
+        {
+            array[i, j] = new Random().Next(LeftRange, RightRange);
+
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+// Получить от пользователя m b n
+
+int m = EnterNumber("Введите значение m: ");
+int n = EnterNumber("Введите значение n: ");
+
+// Создать двумерный массив
+int[,] matrix = CreateRandomArray(m, n, 0, 10);
+// ВЫвести массив
+ShowArray(matrix);
+
+// Получить от пользователя i j индекс
+int i = EnterNumber("Введите значение i: ");
+int j = EnterNumber("Введите значение j: ");
+
+
+// Вывести элемент с заданной позицией или указание, что такого элемента нет.
+if ((i < m && i > -1) && (j < n && j > -1))
+    Console.WriteLine(matrix[i, j]);
+
+else
+    Console.WriteLine($"Элемента с заданной позицией не существует");
